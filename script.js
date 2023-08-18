18481ba3b756837bb2e8b19a3c2fd7c8c1530073
Kyle Angelich <kyleangelich1@gmail.com>
2023-08-17 18:30:36 -0700

    Add function to push books to library

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const theHobbit = {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pageCount: 295,
-    readBook: "not read yet",
-    info: function() {
-        return this.title + " by " + this.author + ", " + this.pageCount + " pages, " + this.readBook;
-    }
-};
+// Book objects will be stored in this array
+const myLibrary = [];
+console.log(myLibrary);
 
-console.log(theHobbit.info());
+function Book(title, author, pageCount, bookRead) {
+    this.title = title
+    this.author = author
+    this.pageCount = pageCount
+    this.bookRead = bookRead
+}
+
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
+// console.log(book1.title);
+
+function addBookToLibrary() {
+    myLibrary.push(book1);
+}
+addBookToLibrary();
