360dd2e689cd3b07852e710186d4d59df1cbdca2
Kyle Angelich <kyleangelich1@gmail.com>
2023-08-18 17:48:08 -0700

    Add function to display book properties in the DOM

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookCardContainer = document.getElementById("book-card-container");
 
 /**
  * Constructor function that represents a book.
@@ -8,22 +9,28 @@ const myLibrary = [];
  * @param {string} bookRead - The status of whether the book has been read or not.
  */
 function Book(title, author, pageCount, bookRead) {
-    this.title = title
-    this.author = author
-    this.pageCount = pageCount
-    this.bookRead = bookRead
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.bookRead = bookRead;
 }
 
-// Static example book.
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-const book2 = new Book("The Alchemist", "Paulo Coelho", 163, "have read/completed");
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Have not read yet");
+const book2 = new Book("The Alchemist", "Paulo Coelho", 163, "Have read/completed");
 
 /**
- * Adds a new book to myLibrary.
+ * Pushes books to myLibrary.
  */
-function addBookToLibrary(book1) {
-    myLibrary.push(book1);
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 console.log(myLibrary);
+
+// Loops through myLibrary and creates a new paragraph for each array item, with object properties displayed.
+myLibrary.forEach(function(book) {
+    const bookCard = document.createElement("p");
+    bookCard.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pageCount}, Read Status: ${book.bookRead}`;
+    bookCardContainer.appendChild(bookCard);
+});
